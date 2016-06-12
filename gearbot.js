72632c23d63de18b89bfca70c4869b2f6179cb04
Ignacio Sanchez Gines <nacho@geardome.com>
2016-06-13 00:14:29 +0200

    alias creation and definition

--- gearbot.js
+++ gearbot.js
@@ -20,6 +20,7 @@ var chance = new Chance();
 
 var controller = Botkit.slackbot({
     debug: false,
+    json_file_store: 'gearbot.db'
 });
 
 var bot = controller.spawn({
@@ -36,6 +37,14 @@ controller.hears(['ayuda', 'puedes hacer', 'sabes hacer'],'direct_message,direct
         bot.reply(message,'Prueba a preguntarme así:');
         bot.reply(message,':small_blue_diamond:  ¿Cómo se dice \"pepinillos\" en inglés?');
         bot.reply(message,':small_blue_diamond:  Traduce \"ring\" a español');
+
+    } else if (message.text.search('alias') >= 0) {
+
+        bot.reply(message,'ok');
+        bot.reply(message,'Puedo aprender a sustituir palabras o frases así:');
+        bot.reply(message,':small_blue_diamond: Cuando diga nginx quiero decir busca la imágen Docker de Nginx');
+        bot.reply(message,':small_blue_diamond: Cuando digo shipit quiero decir haz una release del job myApp');
+        bot.reply(message,':small_blue_diamond: Dime todos mis alias');
 
     } else if (message.text.search('(docker|image|imagen|registry)') >= 0) {
 
@@ -61,11 +70,58 @@ controller.hears(['ayuda', 'puedes hacer', 'sabes hacer'],'direct_message,direct
     } else {
         bot.reply(message,'Especifica un poquito más :kissing_heart:');
         bot.reply(message,'Puedo ayudarte con estos temas:');
+        bot.reply(message,':small_blue_diamond: Aprender un alias');
         bot.reply(message,':small_blue_diamond: Traducir un texto');
         bot.reply(message,':small_blue_diamond: Buscar un jar en Maven Central');
         bot.reply(message,':small_blue_diamond: Buscar en el Docker Registry');
         bot.reply(message,':small_blue_diamond: Trabajar con Jenkins');
     }
+
+});
+
+controller.hears(['cuando dig(a|o) (.*) qui(ero|se) decir (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    var alias_matches = message.original_text.match(/cuando dig(a|o) (.*) qui(ero|se) decir (.*)/i);
+    var alias_key = alias_matches[2];
+    var alias_value = alias_matches[4];
+
+    controller.storage.users.get(message.user,function(err, user) {
+
+        if (user) {
+            user.alias[alias_key] = alias_value;
+        }
+        else {
+            user = {'id': message.user, 'alias':{[alias_key]:alias_value}};
+        }
+
+        controller.storage.users.save(user,function(err, id) {
+            bot.reply(message,'ok');
+            bot.reply(message, 'A partir de ahora \"' + alias_key + '\" = \"' + alias_value + '\"');
+        });
+    });
+
+});
+
+controller.hears(['(dime|muestra.*|lista.*) (.*) alias'],'direct_message,direct_mention,mention',function(bot, message) {
+
+    console.log(message.original_text);
+
+    controller.storage.users.get(message.user,function(err, user) {
+
+        if (user) {
+            bot.reply(message,'ok');
+            bot.reply(message,'Estos son los alias que me has enseñado:');
+
+            for(var alias_key in user.alias) {
+                var alias_value = user.alias[alias_key];
+                bot.reply(message,':small_blue_diamond: \"' + alias_key + '\" = \"' + alias_value + '\"');
+            }
+        }
+        else {
+            bot.reply(message,'no');
+            bot.reply(message,'Aún no me has enseñado ningún alias :unamused:');
+        }
+    });
 
 });
 
@@ -132,29 +188,42 @@ controller.middleware.receive.use(function(bot, message, next) {
 
     if (message.type == 'message') {
 
-        message.text = message.text.toLowerCase();
-        message.text = message.text.replace('/[àáâãäå]/g','a');
-        message.text = message.text.replace('/[èéêë]/g','e');
-        message.text = message.text.replace('/[ìíîï]/g)','i');
-        message.text = message.text.replace('/[òóôõö]/g)','o');
-        message.text = message.text.replace('/[ùúûü]/g','u');
-        message.text = message.text.replace('"','##');
-        message.text = message.text.replace('“','##');
-        message.text = message.text.replace('”','##');
+        controller.storage.users.get(message.user,function(err, user) {
 
-        if (message.subtype == 'channel_join') {
+            if (user) {
+                for(var alias_key in user.alias) {
+                    var alias_value = user.alias[alias_key];
+                    var re = new RegExp(alias_key, 'g');
+                    message.text = message.text.replace(re, alias_value);
+                }
+            }
 
-            bot.say( {
-                text: 'hi',
-                channel: message.channel
-            });
-        }
+            message.original_text = message.text;
+
+            message.text = message.text.toLowerCase();
+            message.text = message.text.replace('/[àáâãäå]/g','a');
+            message.text = message.text.replace('/[èéêë]/g','e');
+            message.text = message.text.replace('/[ìíîï]/g)','i');
+            message.text = message.text.replace('/[òóôõö]/g)','o');
+            message.text = message.text.replace('/[ùúûü]/g','u');
+            message.text = message.text.replace('"','##');
+            message.text = message.text.replace('“','##');
+            message.text = message.text.replace('”','##');
+
+            if (message.subtype == 'channel_join') {
+
+                bot.say( {
+                    text: 'hi',
+                    channel: message.channel
+                });
+            }
+
+            console.log('After receive middleware ' + JSON.stringify(message, null, 4));
+
+            next();
+
+        });
     }
-
-    console.log('After receive middleware ' + JSON.stringify(message, null, 4));
-
-    next();
-
 });
 
 controller.middleware.send.use(function(bot, message, next) {
